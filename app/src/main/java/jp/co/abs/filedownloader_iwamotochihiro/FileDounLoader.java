52d0368bb9d34bee475b520d1970108d200d7585
chihirons <chihi60323@icloud.com>
2018-05-10 13:53:05 +0900

    Test
    aasaa

---
+++ app/src/main/java/jp/co/abs/filedownloader_iwamotochihiro/FileDounLoader.java
@@ -0,0 +1,14 @@
+package jp.co.abs.filedownloader_iwamotochihiro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FileDounLoader extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_doun_loader);
+    }
+}
+//
